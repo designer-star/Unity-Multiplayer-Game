43d637066256b7b6afe3d4f2e0467aaf2dbe2b4c
Oleg <bummer46@gmail.com>
2022-03-29 22:39:45 +0200

    Setting Up Multiple Weapons

---
+++ Multiplayer(Course1)/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public bool isAutomatic;
+    public float timeBetwenSots = .1f, heatPerShot = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
