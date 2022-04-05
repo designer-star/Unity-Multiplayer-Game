a60b9ebbaed2cd222c71a2c8208a13a25ef96255
Oleg <bummer46@gmail.com>
2022-04-05 20:24:42 +0200

    Connecting To The Server

---
+++ Multiplayer(Course1)/Assets/Scripts/Launcher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+
+    public GameObject loadingScreen;
+
+    public GameObject menuButtons;
+
+    public TMP_Text loadingText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseMenu();
+
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to Network...";
+
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void CloseMenu()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+
+        loadingText.text = "Joing Lobyy...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenu();
+        menuButtons.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
