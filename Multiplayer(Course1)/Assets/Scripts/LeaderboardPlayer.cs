b99ffd81b77fbcc568c6e92ef233f3ee353aa2dd
Oleg <bummer46@gmail.com>
2022-04-19 21:15:23 +0200

    75. Laying Out The Leaderboard

---
+++ Multiplayer(Course1)/Assets/Scripts/LeaderboardPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LeaderboardPlayer : MonoBehaviour
+{
+    public TMP_Text playerNameText, killsText, deathsText;
+
+    public void SetDetails(string name, int kills, int deaths)
+    {
+        playerNameText.text = name;
+        killsText.text = kills.ToString();
+        deathsText.text = kills.ToString();
+    }
+}
